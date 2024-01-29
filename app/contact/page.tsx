542582ad65b2bcf7762394e4003d65dc79058325
Cheng (William) Huang <49571870+clwillhuang@users.noreply.github.com>
2024-01-29 18:52:06 -0500

    Styling fixes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,14 @@
 import Layout from '@/components/Layout/Layout';
 import customizeMetadata from '@/components/Head/Head';
-import PaddedLayout from '@/components/PaddedLayout/PaddedLayout';
 import 'animate.css'
+import styles from '@/components/PaddedLayout/PaddedLayout.module.css'
 import ContactForm from '@/components/ContactForm/ContactForm';
 
 export default async function Team() {
     return (
         <Layout>
-            <PaddedLayout addNavbarPadding>
-                <ContactForm />
-            </PaddedLayout>
+            <div className={styles.navbarPadding}/>
+            <ContactForm />
         </Layout>
     )
 }
